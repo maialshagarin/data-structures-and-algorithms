bd87d6996982b534effaac5114abef7612e8a682
mai <mai.yusuf28@yahoo.com>
2019-11-11 23:28:09 +0200

    the fourth challenge done

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -63,7 +63,9 @@ console.log(people[1].isAuthor) prints true
 ------------------------------------------------------------------------------------------------ */
 
 const setStatusAsAuthor = (people) => {
-  // Solution code here...
+ people.forEach(element => {
+    element.isAuthor = true ; 
+ }); // Solution code here...
 };
 
 /* ------------------------------------------------------------------------------------------------
